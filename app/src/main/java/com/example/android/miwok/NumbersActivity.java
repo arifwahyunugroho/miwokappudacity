fa2fe9b835c150debe1da143deb51ed0f507ad6c
Arif Wahyu Nugroho <ariev.noegroho@gmail.com>
2017-02-08 23:49:55 +0700

    Menyelesaikan BAB 2

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,7 +2,6 @@ package com.example.android.miwok;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -12,20 +11,7 @@ public class NumbersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
-
-//        ArrayList<String> words = new ArrayList<>();
-//        words.add("one");
-//        words.add("two");
-//        words.add("three");
-//        words.add("four");
-//        words.add("five");
-//        words.add("six");
-//        words.add("seven");
-//        words.add("eight");
-//        words.add("nine");
-//        words.add("ten");
-//        words.add("eleven");
+        setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti"));
@@ -38,27 +24,6 @@ public class NumbersActivity extends AppCompatActivity {
         words.add(new Word("eight", "kawinta"));
         words.add(new Word("nine", "wo’e"));
         words.add(new Word("ten", "na’aacha"));
-
-//        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
-
-//        int index = 0;
-//
-//        while (index<words.size()){
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(index));
-//            rootView.addView(wordView);
-//            index++;
-//        }
-
-//        for (int index=0; index<words.size();index++){
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(index));
-//            rootView.addView(wordView);
-//        }
-
-//        ArrayAdapter<Word> itemsAdapter = new ArrayAdapter<Word>(this, R.layout.list_item, words);
-//        ListView listView = (ListView) findViewById(R.id.list);
-//        listView.setAdapter(itemsAdapter);
 
         WordAdapter adapter =  new WordAdapter(this, words);
         ListView listView = (ListView) findViewById(R.id.list);
